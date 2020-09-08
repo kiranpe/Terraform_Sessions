ce858d0066dec4fa010338bbf8ae44a330567f5d
kiran <kiranwls2011@gmail.com>
2020-09-08 14:29:46 +0530

    first ec2 instance

---
+++ Deploy/first-ec2.tf
@@ -0,0 +1,14 @@
+### Important Note
+
+Note: Every region has a different AMI ID. Make sure you use the latest AMI ID from the AWS console.
+
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "PUT-YOUR-ACCESS-KEY-HERE"
+  secret_key = "PUT-YOUR-SECRET-KEY-HERE"
+}
+
+resource "aws_instance" "myec2" {
+   ami = "ami-082b5a644766e0e6f"
+   instance_type = "t2.micro"
+}
